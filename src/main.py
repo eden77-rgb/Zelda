dff3af285372f7b2c69f5e0de7ddf71cee40bb8b
Eden <eden.vandewatyne@estiam.com>
2025-08-11 23:44:11 +0200

    Affichage de la carte

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 import pygame
+import pytmx
+import pyscroll
 pygame.init()
 
 WIDTH = 256
@@ -6,6 +8,13 @@ HEIGHT = 224
 SCALE = 2
 
 screen = pygame.display.set_mode((WIDTH * SCALE, HEIGHT * SCALE))
+
+tmx_data = pytmx.util_pygame.load_pygame("../maps/main.tmx")
+map_data = pyscroll.TiledMapData(tmx_data)
+map_layer = pyscroll.BufferedRenderer(map_data, screen.get_size(), clamp_camera=False)
+
+group = pyscroll.PyscrollGroup(map_layer=map_layer)
+group.draw(screen)
 
 running = True
 while running:
