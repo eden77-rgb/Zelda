648215afec68967d662c276f629a44701aef9b8b
Eden <eden.vandewatyne@estiam.com>
2025-08-12 00:18:41 +0200

    Ajout de data + Ajout de JsonLoader + Ajout d'une Camera avec déplacement bloquer

---
+++ assets/tilesets/main-carte.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="main-carte" tilewidth="32" tileheight="32" tilecount="20480" columns="128">
+ <image source="../zelda/Overworld/Light World/SNES - The Legend of Zelda_ A Link to the Past - Overworld - Light World.png" trans="808080" width="4110" height="5136"/>
+</tileset>
