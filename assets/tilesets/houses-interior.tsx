60003c14988fbb7b66a5055457305e60a6e5f7e0
Eden <eden.vandewatyne@estiam.com>
2025-08-22 17:24:58 +0200

    Création de la carte Link-house

---
+++ assets/tilesets/houses-interior.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="houses-interior" tilewidth="16" tileheight="16" tilecount="1372" columns="28">
+ <image source="../zelda/Tilesets/Houses Interior Tiles/SNES - The Legend of Zelda_ A Link to the Past - Tilesets - Houses Interior Tiles.png" trans="ffffff" width="454" height="790"/>
+</tileset>
